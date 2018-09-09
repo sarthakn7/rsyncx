728c1e525fb89e9d2f6c3a4c0bb6746b4b44debd
Sarthak Nandi <sarthakn@gmail.com>
2018-09-08 22:08:41 -0400

    Added module to find the required operations

--- changefinder/findchanges.go
+++ changefinder/findchanges.go
@@ -1,6 +1,175 @@
 package changefinder
 
+import (
+	"crypto/sha256"
+	"fmt"
+	"github.com/sarthakn7/rsyncx/commons"
+	"github.com/sger/go-hashdir"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+)
+
 // Generate a file containing the planned changes
 func FindChanges(source *string, destination *string, changeFile *string) {
+	emptyPath := ""
+	sourceMetadata := createDirectoryMetadata(source, &emptyPath)
+	destinationMetadata := createDirectoryMetadata(destination, &emptyPath)
 
+	findRequiredOperations(sourceMetadata, destinationMetadata)
+}
+
+func createDirectoryMetadata(directoryPath, relativePathToDir *string) *commons.DirectoryMetadata {
+	name := filepath.Base(*directoryPath)
+	hash := createDirectoryHash(directoryPath)
+
+	subdirectories := make([]*commons.DirectoryMetadata, 0)
+	files := make([]*commons.FileMetadata, 0)
+
+	fileNameToFile := make(map[*string]*commons.FileMetadata)
+	subDirectoryNameToSubDirectory := make(map[*string]*commons.DirectoryMetadata)
+
+	currentRelativePathToDir := filepath.Join(*relativePathToDir, name)
+
+	err := filepath.Walk(*directoryPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("Unable to read path %q: %v\n", path, err)
+			return err
+		}
+		if info.IsDir() {
+			metadata := createDirectoryMetadata(&path, &currentRelativePathToDir)
+			directoryName := filepath.Base(path)
+			subDirectoryNameToSubDirectory[&directoryName] = metadata
+			subdirectories = append(subdirectories, metadata)
+		} else {
+			metadata := createFileMetadata(&path, &currentRelativePathToDir, info.ModTime(), info.Size())
+			fileName := filepath.Base(path)
+			fileNameToFile[&fileName] = metadata
+			files = append(files, metadata)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("Error walking the path %q: %v\n", directoryPath, err)
+	}
+
+	return &commons.DirectoryMetadata{&name, directoryPath, hash, fileNameToFile, subDirectoryNameToSubDirectory, files, subdirectories}
+}
+
+func createFileMetadata(filePath, relativePathToDir *string, modTime time.Time, size int64) *commons.FileMetadata {
+	name := filepath.Base(*filePath)
+	hash := createFileHash(filePath)
+
+	return &commons.FileMetadata{&name, filePath, relativePathToDir, hash, modTime, size}
+}
+
+func createDirectoryHash(directoryPath *string) []byte {
+	hash, err := hashdir.Create(*directoryPath, "sha256")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return []byte(hash)
+}
+
+func createFileHash(filePath *string) []byte {
+	file, err := os.Open(*filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	hasher := sha256.New()
+	if _, err := io.Copy(hasher, file); err != nil {
+		log.Fatal(err)
+	}
+	return hasher.Sum(nil)
+}
+
+type fileSearchParams struct {
+	name    *string
+	hash    []byte
+	modTime time.Time
+	size    int64
+}
+
+// Creates a mapping from fileSearchParams to FileMetadata for all files in the directory
+func createInvertedFileMapping(directoryMetadata *commons.DirectoryMetadata) map[*fileSearchParams]*commons.FileMetadata {
+	invertedMapping := make(map[*fileSearchParams]*commons.FileMetadata)
+
+	addFilesToInvertedMapping(directoryMetadata, invertedMapping)
+
+	return invertedMapping
+}
+
+func addFilesToInvertedMapping(directoryMetadata *commons.DirectoryMetadata, invertedMapping map[*fileSearchParams]*commons.FileMetadata) {
+	for _, file := range directoryMetadata.Files {
+		fileParam := createFileSearchParam(file)
+		invertedMapping[fileParam] = file
+	}
+
+	for _, directory := range directoryMetadata.Subdirectories {
+		addFilesToInvertedMapping(directory, invertedMapping)
+	}
+}
+
+func createFileSearchParam(file *commons.FileMetadata) *fileSearchParams {
+	return &fileSearchParams{file.Name, file.Hash, file.ModTime, file.Size}
+}
+
+func findRequiredOperations(sourceMetadata, destinationMetadata *commons.DirectoryMetadata) {
+	invertedSourceMapping := createInvertedFileMapping(sourceMetadata)
+	invertedDestinationMapping := createInvertedFileMapping(destinationMetadata)
+
+	operations := make([]*commons.Operation, 0)
+
+	// TODO: directory creation/deletion ops
+	findAddMoveOperationsInDirectory(sourceMetadata, destinationMetadata, invertedDestinationMapping, operations)
+	findDeleteOperationsInDirectory(destinationMetadata, invertedSourceMapping, operations)
+}
+
+func findAddMoveOperationsInDirectory(sourceMetadata, destinationMetadata *commons.DirectoryMetadata,
+	invertedDestinationMapping map[*fileSearchParams]*commons.FileMetadata, operations []*commons.Operation) {
+
+	for _, sourceFile := range sourceMetadata.Files {
+		param := createFileSearchParam(sourceFile)
+		destinationFile, found := invertedDestinationMapping[param]
+
+		if !found {
+			// Add file addition operation
+			op := commons.Operation{commons.OpAddFile, sourceFile.CompletePath, sourceFile.RelativePathToDir}
+			operations = append(operations, &op)
+		} else {
+			// Add file move operation
+			op := commons.Operation{commons.OpMoveFile, destinationFile.CompletePath, sourceFile.RelativePathToDir}
+			operations = append(operations, &op)
+		}
+	}
+
+	for _, subDirectory := range sourceMetadata.Subdirectories {
+		findAddMoveOperationsInDirectory(subDirectory, destinationMetadata, invertedDestinationMapping, operations)
+	}
+}
+
+func findDeleteOperationsInDirectory(destinationMetadata *commons.DirectoryMetadata,
+	invertedSourceMapping map[*fileSearchParams]*commons.FileMetadata, operations []*commons.Operation) {
+
+	for _, destinationFile := range destinationMetadata.Files {
+		param := createFileSearchParam(destinationFile)
+		_, found := invertedSourceMapping[param]
+
+		if !found {
+			// Add file deletion operation
+			op := commons.Operation{commons.OpDeleteFile, destinationFile.CompletePath, nil}
+			operations = append(operations, &op)
+		}
+	}
+
+	for _, subDirectory := range destinationMetadata.Subdirectories {
+		findDeleteOperationsInDirectory(subDirectory, invertedSourceMapping, operations)
+	}
 }
---
+++ commons/commons.go
@@ -0,0 +1,39 @@
+package commons
+
+import "time"
+
+type FileMetadata struct {
+	Name              *string
+	CompletePath      *string // Complete path on disk
+	RelativePathToDir *string // Path relative to source/destination directory
+	Hash              []byte
+	ModTime           time.Time
+	Size              int64
+}
+
+type DirectoryMetadata struct {
+	Name                           *string
+	Path                           *string
+	Hash                           []byte
+	FileNameToFile                 map[*string]*FileMetadata
+	SubDirectoryNameToSubDirectory map[*string]*DirectoryMetadata
+	Files                          []*FileMetadata
+	Subdirectories                 []*DirectoryMetadata
+}
+
+type OpType int
+
+// Possible file operation types
+const (
+	OpAddFile OpType = iota + 1
+	OpDeleteFile
+	OpMoveFile
+	OpCreateDirectory
+	OpDeleteDirectory
+)
+
+type Operation struct {
+	opType      OpType
+	source      *string
+	destination *string
+}
